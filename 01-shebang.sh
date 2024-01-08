be8c7bffd5bed79ea6de8770705cf7328b5eee8c
23-Pravallika <learngain23@gmail.com>
2024-01-08 17:52:09 +0530

    AutoPush

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -7,3 +7,4 @@ echo -e "line3\n\tline4"
 # color
 echo -e "\e[35m nice color magenta \e[0m"
 echo -e "\e[36m printing cyan \e[0m"
+echo -e "\e[36m,45m printing font colr-cyan and background color-magenta \e[0m"
