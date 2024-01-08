7d7a96841c200a36919080601244f5d8bbdb2edd
23-Pravallika <learngain23@gmail.com>
2024-01-08 17:56:37 +0530

    AutoPush

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -7,5 +7,5 @@ echo -e "line3\n\tline4"
 # color
 echo -e "\e[35m nice color magenta \e[0m"
 echo -e "\e[36m printing cyan \e[0m"
-echo -e "\e[36m;45m printing font colr as cyan and background color as magenta \e[0m"
+echo -e "\e[36;45m printing font colr as cyan and background color as magenta \e[0m"
 echo -e "\e[43;32m I am printing Green Color with YELLOW as Background \e[0m"
