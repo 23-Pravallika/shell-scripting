ef7faa61ffb0589059f97966aeb33ee128408766
23-Pravallika <learngain23@gmail.com>
2024-01-10 17:45:31 +0530

    AutoPush

--- 09-more-in-forloop.sh
+++ 09-more-in-forloop.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-for var in {}
+
+<<COMMENT
+  We can use the for loop with the range syntax and indicate the first and last element.
+   
+   Syntax: for varabile in {START..END}
+
+COMMENT
+
+for var in {1..6}
+    do
+        echo "{START..END}"
+    done
+
+<<COMMENT
+COMMENT
+
+a=1
+b=10
+for i in {$a..$b}   #loop 1
+do
+    echo "$i"
+done
