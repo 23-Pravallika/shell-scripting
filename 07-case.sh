0863aef363a53e353176713901368be9a03b6ff0
23-Pravallika <learngain23@gmail.com>
2024-01-10 16:53:08 +0530

    AutoPush

--- 07-case.sh
+++ 07-case.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 Action=$1
+Option=$1
 
 case $Action in
   Add)
@@ -18,3 +19,16 @@ case $Action in
     echo  -e "\e[31m Invalid option Pls enter the valid option \e[0m"    
 
 esac
+
+
+case $Option in
+    Addition)
+        echo "Addition of numbers: $(4+1)"
+        ;;
+    Mulitiplication)
+        echo "Mulitiplication of numbers :$(2*3)"
+        ;;
+    *)
+
+    echo -e "\e[31m Invalid Option \e[0m"
+esac    
