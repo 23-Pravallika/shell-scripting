8f45fb404ebe1e1e0182b2a3ba3c5895a731f47e
23-Pravallika <learngain23@gmail.com>
2024-01-10 14:42:41 +0530

    AutoPush

--- 06-simple-function.sh
+++ 06-simple-function.sh
@@ -5,7 +5,7 @@ funct() {
 }
 
 fu() {
-    echo "TodayDate $(date +%D)"
+    echo "TodayDate : $(date +%D)"
     echo -e "\e[32m Load Average On The system is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}') \e[0m"
     echo -e "\t Total no.of session : $(who | wc -l)"
     funct
