1b87e5634086a0cc8699f342f60e965b54984dd4
23-Pravallika <learngain23@gmail.com>
2024-01-10 14:40:44 +0530

    AutoPush

--- 06-simple-function.sh
+++ 06-simple-function.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+funct() {
+    echo "I am a messaged called from sample function"
+}
+
+fu() {
+    echo "TodayDate $(date +%D)"
+    echo -e "\e[32m Load Average On The system is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}') \e[0m"
+    echo -e "\t Total no.of session : $(who | wc -l)"
+    funct
+}
+
+fun
