3225b795e233e737da24c52a08d2348129cc2c83
23-Pravallika <learngain23@gmail.com>
2024-01-11 19:41:24 +0530

    AutoPush

--- robo/components/frontend.sh
+++ robo/components/frontend.sh
@@ -2,6 +2,13 @@
 
 echo "frontend :"
 set -e
+
+ID=$(id root)
+
+if [ $ID -ne 0 ]; then
+echo "\e[31m You need to be root to perform this command \e[0m"
+fi
+
 yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
