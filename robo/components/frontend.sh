9e2c4c7fcd5e036282d046720e3ec977359818ae
23-Pravallika <learngain23@gmail.com>
2024-01-12 12:01:56 +0530

    AutoPush

--- robo/components/frontend.sh
+++ robo/components/frontend.sh
@@ -8,20 +8,21 @@ ID=$(id -u)
 LOGFILE="/tmp/frontend.logs"
 
 if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m You should execute this script as root user or with a sudo as prefix  \e[0m"
-    $?
+    echo -e  "\e[31m You should execute this script as root user or with a sudo as prefix  \e[0m"
+    #exit 1
 fi
 
 status() {
     
     if [ $1 -eq 0 ] ; then
-        echo -e "\e[32m Success \e[0m"
+        echo -e  "\e[32m Success \e[0m"
     else
-        echo -e "\e[31m Failure \e[0m"
+        echo -e  "\e[31m Failure \e[0m"
+        exit 2
 fi
 }
 
-echo "Installing nginx : "
+echo -n "Installing nginx : "
 yum install nginx -y   &>> $LOGFILE
 status $?
 systemctl enable nginx  &>> $LOGFILE
@@ -29,11 +30,11 @@ systemctl start nginx   &>> $LOGFILE
 status $?
 
 
-echo "downloading content :"
+echo -n "downloading content :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"  &>> $LOGFILE
 status $?
 
-echo "Cleaning and deploying :"
+echo -n "Cleaning and deploying :"
 cd /usr/share/nginx/html
 rm -rf *     
 unzip /tmp/frontend.zip  &>> $LOGFILE
@@ -44,7 +45,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 status $?
 
 
-echo "restarting the service :"
+echo -n "restarting the service :"
 systemctl daemon-reload
 systemctl restart nginx
 status $?
