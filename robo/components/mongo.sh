1c7222e2813e200583a0285b727b195afc7d872b
23-Pravallika <learngain23@gmail.com>
2024-01-12 12:53:24 +0530

    AutoPush

--- robo/components/mongo.sh
+++ robo/components/mongo.sh
@@ -1,4 +1,55 @@
 #!/bin/bash
 
-echo "Iam mongo :"
+ID=$(id -u)
+LOGFILES="/tmp/mongo.logs"
+
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m You should execute this script as root user or with a sudo as prefix  \e[0m"
+    exit
+fi
+
+stauts() {
+    if [$1 eq 0] ; then
+        echo -e "\e[32m Sucess \e[0m"
+    else
+        echo -e "\e[31m Failure \e[0m"
+        exit
+}
+
+echo -n "Installing the Repos : "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo  &>> $LOGFILES
+status $?
+
+echo -n "Installing the mongo : "
+yum install -y mongodb-org  &>> $LOGFILES
+status $?
+
+echo -n "Starting mongo : "
+systemctl enable mongod   &>> $LOGFILES
+systemctl start mongod
+status $?
+
+echo -n "Updating IP address : "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status $?
+
+echo -n "Restarting Service : "
+systemctl daemon-reload
+systemctl restart mongod
+status $?
+
+echo -n "Downloading the schema : "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"   &>> $LOGFILES
+status $?
+
+echo -n "Unzipping the downloaded schema : "
+cd /tmp
+unzip mongodb.zip &>> $LOGFILES
+status $?
+
+echo -n "Injecting the schema : "
+cd mongodb-main
+mongo < catalogue.js  &>> $LOGFILES
+mongo < users.js  &>> $LOGFILES
+status $?
 
