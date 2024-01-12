fc7e6181e29225ae4c5cd3eb17b5368fbc668c5b
23-Pravallika <learngain23@gmail.com>
2024-01-12 18:44:31 +0530

    AutoPush

--- robo/components/catalogue.sh
+++ robo/components/catalogue.sh
@@ -21,17 +21,25 @@ echo -n "Configuring the nodejs repo :"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>> $LOGFILE
 stat $?  
 
+
 echo -n "Installing NodeJS :"
 yum install nodejs -y &>> $LOGFILE
 stat $?
 
-echo -n "Creating Application user account :"
-useradd roboshop
-status $?
+
+id roboshop &>> $LOGFILE
+if [id -ne 0] ; then
+    echo -n "Creating Application user account :"
+    useradd roboshop
+    status $?
+fi
+
 
 echo -n "Downloading the catalogue component :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"  &>> $LOGFILE
 status $?
+
+
 
 echo -n "Extracting the catalogue :"
 cd /home/roboshop
@@ -39,14 +47,20 @@ unzip -o /tmp/catalogue.zip   &>> $LOGFILE
 mv catalogue-main catalogue
 status $?
 
+
+
 echo -n "Installing the dependencies :"
 cd /home/roboshop/catalogue
 npm install  &>> $LOGFILE
 status $?
 
+
+
 echo -n "IP address Upate :"
 sed -i -e 's/MONGO_DNSNAME/172.31.31.62/' /home/roboshop/catalogue/systemd.service  &>> $LOGFILE
 status $?
+
+
 
 echo -n "Starting the catalogue :"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
