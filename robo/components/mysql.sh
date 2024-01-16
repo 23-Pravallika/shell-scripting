7d2443fd6ee6730658ff143a50c2482f1d4de4a1
23-Pravallika <learngain23@gmail.com>
2024-01-16 19:35:27 +0530

    AutoPush

--- robo/components/mysql.sh
+++ robo/components/mysql.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-echo "Iam mysql :"
+COMPONENT=mysql
+source component/common.sh
+
+
+echo -n "Configuring the $COMPONENT repo :"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo &>> $LOGFILE  
+status $?
+
+echo -n "Installing the $COMPONENT :"
+yum install mysql-community-server -y
+status $?
+
+echo -n "Starting the $COMPONENT :"
+systemctl enable mysqld &>> $LOGFILE
+systemctl start mysqld
+status $?
 
