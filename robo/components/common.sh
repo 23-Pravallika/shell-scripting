b9560919a93054c66eafbb81c636ac7e26f49a02
23-Pravallika <learngain23@gmail.com>
2024-01-16 13:58:26 +0530

    AutoPush

--- robo/components/common.sh
+++ robo/components/common.sh
@@ -33,13 +33,13 @@ CREATE_USER(){
 Download_EXTRACT(){
     echo -n "Downloading the $COMPONENT component :"
     curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-    stat $? 
+    status $? 
 
-    echo -n "Extracting the $COMPONENT in the $APPUSER directory"
+    echo -n "Extracting the $COMPONENT in the $APPUSER directory :"
     cd /home/$APPUSER 
     rm -rf /home/$APPUSER/$COMPONENT &>> $LOGFILE
     unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-    stat $? 
+    status $? 
 
     echo -n "Configuring the permissions :"
     mv /home/$APPUSER/$COMPONENT-main  /home/$APPUSER/$COMPONENT
