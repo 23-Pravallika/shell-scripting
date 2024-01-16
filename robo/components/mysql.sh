d7c8ccae3a6582b6210f53539f308a5a52eceb87
23-Pravallika <learngain23@gmail.com>
2024-01-16 21:02:26 +0530

    AutoPush

--- robo/components/mysql.sh
+++ robo/components/mysql.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
+#!/bin/bash 
 
 COMPONENT=mysql
 source components/common.sh
 
-
 echo -n "Configuring the $COMPONENT repo :"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo &>> $LOGFILE  
-status $?
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo 
+stat $?
 
 echo -n "Installing the $COMPONENT :"
-yum install mysql-community-server -y   &>> $LOGFILE
-status $?
+yum install mysql-community-server -y  &>> $LOGFILE  
+stat $? 
 
-echo -n "Starting the $COMPONENT :"
-systemctl enable mysqld &>> $LOGFILE
-systemctl start mysqld
-status $?
+echo -n "Starting $COMPONENT :" 
+systemctl enable mysqld  &>> $LOGFILE  
+systemctl start mysqld   &>> $LOGFILE  
+stat $?
 
 echo -n "Grab $COMPONENT default password :"
-DEFAULT_ROOT_PWD=$(grep "temporary password"  /var/log/mysqld.log | awk -F : '{print $NF}')
-status $?
+DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
+stat $? 
 
 # This should only run for the first time or when the default password is not changed.
 echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
@@ -31,23 +30,23 @@ if [ $? -ne 0 ] ; then
 
 fi 
 
-
 # Ensure you run this only when the password validate plugin exist 
-echo "show plugins;" |  mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE
-if [ $? -eq 0 ] ; then
+echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE 
+if [ $? -eq 0 ] ; then 
+
     echo -n "Uninstalling Password Validation Plugin :"
     echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
     stat $?
-fi
 
+fi 
 
 echo -n "Downloading the $COMPONENT Schema :"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
-stat $?
+stat $? 
 
 echo -n "Extracting the $COMPONENT Schema :"
-cd /tmp
-unzip $COMPONENT.zip
+cd /tmp 
+unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
 stat $? 
 
 echo -n "Injecting the schema :"
