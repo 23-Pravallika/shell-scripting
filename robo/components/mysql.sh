4ffc54c5374ce6fc16fd34a6dec47998ade42bb3
23-Pravallika <learngain23@gmail.com>
2024-01-16 21:06:11 +0530

    AutoPush

--- robo/components/mysql.sh
+++ robo/components/mysql.sh
@@ -5,20 +5,20 @@ source components/common.sh
 
 echo -n "Configuring the $COMPONENT repo :"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo 
-stat $?
+status $?
 
 echo -n "Installing the $COMPONENT :"
 yum install mysql-community-server -y  &>> $LOGFILE  
-stat $? 
+status $?
 
 echo -n "Starting $COMPONENT :" 
 systemctl enable mysqld  &>> $LOGFILE  
 systemctl start mysqld   &>> $LOGFILE  
-stat $?
+status $?
 
 echo -n "Grab $COMPONENT default password :"
 DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
-stat $? 
+status $?
 
 # This should only run for the first time or when the default password is not changed.
 echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
@@ -26,7 +26,7 @@ if [ $? -ne 0 ] ; then
 
     echo -n "Password Reset of root user :"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
-    stat $?
+    status $?
 
 fi 
 
@@ -36,20 +36,20 @@ if [ $? -eq 0 ] ; then
 
     echo -n "Uninstalling Password Validation Plugin :"
     echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
-    stat $?
+    status $?
 
 fi 
 
 echo -n "Downloading the $COMPONENT Schema :"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
-stat $? 
+status $?
 
 echo -n "Extracting the $COMPONENT Schema :"
 cd /tmp 
 unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
-stat $? 
+status $? 
 
 echo -n "Injecting the schema :"
 cd $COMPONENT-main
 mysql -uroot -pRoboShop@1 < shipping.sql 
-stat $? 
+status $?
