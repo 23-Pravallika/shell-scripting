8daae3690ddbb03f5f7b8f57568a291fcece2840
23-Pravallika <learngain23@gmail.com>
2024-01-16 21:00:35 +0530

    AutoPush

--- robo/components/mysql.sh
+++ robo/components/mysql.sh
@@ -24,10 +24,13 @@ status $?
 # This should only run for the first time or when the default password is not changed.
 echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
 if [ $? -ne 0 ] ; then 
+
     echo -n "Password Reset of root user :"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE  
-    status $?
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
+    stat $?
+
 fi 
+
 
 # Ensure you run this only when the password validate plugin exist 
 echo "show plugins;" |  mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE
