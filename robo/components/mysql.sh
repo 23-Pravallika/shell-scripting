fff891004a0463f592844f9cb554514f83a90290
23-Pravallika <learngain23@gmail.com>
2024-01-16 20:57:36 +0530

    AutoPush

--- robo/components/mysql.sh
+++ robo/components/mysql.sh
@@ -17,3 +17,37 @@ systemctl enable mysqld &>> $LOGFILE
 systemctl start mysqld
 status $?
 
+echo -n "Grab $COMPONENT default password :"
+DEFAULT_ROOT_PWD=$(grep "temporary password"  /var/log/mysqld.log | awk -F : '{print $NF}')
+status $?
+
+# This should only run for the first time or when the default password is not changed.
+echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
+if [ $? -ne 0 ] ; then 
+    echo -n "Password Reset of root user :"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE  
+    status $?
+fi 
+
+# Ensure you run this only when the password validate plugin exist 
+echo "show plugins;" |  mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE
+if [ $? -eq 0 ] ; then
+    echo -n "Uninstalling Password Validation Plugin :"
+    echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
+    stat $?
+fi
+
+
+echo -n "Downloading the $COMPONENT Schema :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+stat $?
+
+echo -n "Extracting the $COMPONENT Schema :"
+cd /tmp
+unzip $COMPONENT.zip
+stat $? 
+
+echo -n "Injecting the schema :"
+cd $COMPONENT-main
+mysql -uroot -pRoboShop@1 < shipping.sql 
+stat $? 
