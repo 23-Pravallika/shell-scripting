7a908eb7956ce2dcd526aab7cd39bfd716201676
23-Pravallika <learngain23@gmail.com>
2024-01-18 21:50:55 +0530

    AutoPush

--- robo/creating-ec2.sh
+++ robo/creating-ec2.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+COMPONENT=$1
+
 Image_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo -n "starting the server :"
 
-aws ec2 run-instances --image-id $Image_ID --instance-type t2.micro | jq
+#running the instance without mentioning the instance name :
+# aws ec2 run-instances --image-id $Image_ID --instance-type t2.micro | jq
+
+#running the instance with the instance name :
+aws ec2 run-instances  --image-id $Image_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=$Image_ID,Value=$COMPONENT}]"  | jq
+
