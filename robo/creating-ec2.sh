5ebdf38095146f7ec1d1db9061f68a4e86593fe1
23-Pravallika <learngain23@gmail.com>
2024-01-18 21:41:18 +0530

    AutoPush

--- robo/creating-ec2.sh
+++ robo/creating-ec2.sh
@@ -4,4 +4,4 @@ Image_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-C
 
 echo -n "starting the server :"
 
-aws ec2 run-instances --image-id $Image_ID --instance-type t2.micro
+aws ec2 run-instances --image-id $Image_ID --instance-type t2.micro | jq
