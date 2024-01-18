2c0cbde51b066a2390ab2390ff71add42cf879da
23-Pravallika <learngain23@gmail.com>
2024-01-18 21:59:16 +0530

    AutoPush

--- robo/creating-ec2.sh
+++ robo/creating-ec2.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# This is a script created to launch EC2 Servers and create the associated Route53 Record 
+
 COMPONENT=$1
 
-Image_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
+echo -n "Ami id is $AMI_ID  :"
 
-echo -n "starting the server :"
+echo -n "Launching the instance with $AMI_ID as AMI :"
 
 #running the instance without mentioning the instance name :
-# aws ec2 run-instances --image-id $Image_ID --instance-type t2.micro | jq
+# aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq
 
 #running the instance with the instance name :
-aws ec2 run-instances  --image-id $Image_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=$Image_ID,Value=$COMPONENT}]"  | jq
+aws ec2 run-instances  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]"  | jq
 
