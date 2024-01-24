4a62c5fd3606bd550bf773eb791cba7260d596c0
23-Pravallika <learngain23@gmail.com>
2024-01-24 15:07:17 +0530

    AutoPush

--- robo/creating-ec2.sh
+++ robo/creating-ec2.sh
@@ -29,7 +29,7 @@ echo "Launching the instance with $AMI_ID as AMI :"
 IP=$(aws ec2 run-instances  --image-id $AMI_ID \
         --instance-type t2.micro \
         --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-        --security-group-ids ${SGID} \
+        --security-group-ids ${sg_ID} \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 echo "PrivateIpAddresses is : $IP "
